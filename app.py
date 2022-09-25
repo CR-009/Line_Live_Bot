a2e01e3da3d429a615692604e3b0187a73b22753
CR-009 <114333483+CR-009@users.noreply.github.com>
2022-09-25 23:12:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 
+from crawl import *
 from linebot.models import *
-from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+from flask import Flask, request, abort, render_template
 
 
 app = Flask(__name__)
 
-Channel_Access_Token = 'iSMOZduFbHkTglNwWIKqaYgzO9B9LL0VpcdV4/1QEgbYrNekuQSJBxDV5yi9yoLquDizYjeux4NZBHOUTx3rwxcSFZaDw+tixor0ZtoUMuFrSipfQZb+JLOY50s8IcF6PAYRwUaJywp9oaN2sz03MgdB04t89/1O/w1cDnyilFU='
+Channel_Access_Token = ''
 line_bot_api    = LineBotApi(Channel_Access_Token)
-Channel_Secret  = '1d60146099c8460565358a489b0a0524'
+Channel_Secret  = ' '
 handler = WebhookHandler(Channel_Secret)
+
 
 # handle request from "/callback" 
 @app.route("/callback", methods=['POST'])
@@ -26,16 +28,30 @@ def callback():
         abort(400)
     return 'OK'
 
+
 # handle text message
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    if event.source.user_id != " ":
+
+    if "新聞" in msg:
+        result = news_crawler()
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=result)
+        )
+    else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=msg)
         )
 
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
